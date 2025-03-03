a7417700495f6c08fa59b25d7d9e0ac79cf7e95e
An256122006 <dinhtrongan2006@gmail.com>
2025-03-03 07:14:48 +0700

    Add files via upload

--- bai9.js
+++ bai9.js
@@ -10,9 +10,13 @@ let c = Number(prompt("moi ban nhap giay : "));
 if(c<0 && c>60){
     let c = Number(prompt("moi ban nhap lai gaiy: "));
 }
-if(b===60 && c===60){
-    a==a+1;
-    b=0;
-    c=0;
+if(a>12){
+    if(b===60 && c===60){
+        a==a+1;
+        b=0;
+        c=0;
+    }
+    alert(a%12+":"+b+":"+c);
+}else{
+    alert(a+":"+b+":"+c);
 }
-alert(a%12+":"+b+":"+c);
